1a5aa1bc461088dcd9da49e6cd0fe7baf9d6109e
leobuck <leobuck12@gmail.com>
2023-07-16 21:35:47 -0300

    Lazy Properties

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -8,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
 
 builder.Services.AddDbContext<FilmeContext>(opts =>
-    opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+    opts.UseLazyLoadingProxies()
+        .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
