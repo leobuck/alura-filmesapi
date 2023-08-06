4ea69897e1819609e1a52e3ca646f3e3adbfafc6
leobuck <leobuck12@gmail.com>
2023-08-05 22:18:17 -0300

    Relacionando Filme e Sessão

--- FilmesAPI/Controllers/SessaoController.cs
+++ FilmesAPI/Controllers/SessaoController.cs
@@ -6,8 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesAPI.Controllers;
 
-[Route("api/[controller]")]
 [ApiController]
+[Route("[controller]")]
 public class SessaoController : ControllerBase
 {
 	private FilmeContext _context;
