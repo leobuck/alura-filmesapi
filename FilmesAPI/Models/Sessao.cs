89609701d82bbc7a5428e8f7090342097608637f
leobuck <leobuck12@gmail.com>
2023-09-30 19:47:17 -0300

    Relacionando com ModelBuilder

--- FilmesAPI/Models/Sessao.cs
+++ FilmesAPI/Models/Sessao.cs
@@ -1,15 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace FilmesAPI.Models;
+﻿namespace FilmesAPI.Models;
 
 public class Sessao
 {
-    [Key]
-    [Required]
-    public int Id { get; set; }
-
-    [Required]
-    public int FilmeId { get; set; }
+    public int? FilmeId { get; set; }
     public virtual Filme Filme { get; set; }
 
     public int? CinemaId { get; set; }
